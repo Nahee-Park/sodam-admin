cabbaab7162ff836e61624a55208c87c8aa21ad1
nahee-park <jessica396@ewhain.net>
2022-06-07 23:57:23 +0900

    feat: 버튼 컴포넌트 제작

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { HTMLAttributes } from 'react';
+import styled from 'styled-components';
+
+interface ButtonStyleProps {
+  width?: string;
+  height?: string;
+}
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement>, ButtonStyleProps {
+  className?: string;
+  children: string;
+  [key: string]: any;
+}
+function Button({ children, width = '100px', height = '30px' }: ButtonProps) {
+  return (
+    <Styled.Button width={width} height={height}>
+      {children}
+    </Styled.Button>
+  );
+}
+
+export default Button;
+
+const Styled = {
+  Button: styled.button<ButtonStyleProps>`
+    border-radius: 8px;
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
+    background-color: #abacfe;
+    color: white;
+    border: none;
+  `,
+};
